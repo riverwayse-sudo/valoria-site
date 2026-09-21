99bfac228156b9ffcf924d66ad7ace290bacf0a7
riverwayse-sudo <riverwayse@gmail.com>
2026-09-21 15:31:41 +0100

    brand: use exact original Valoria logo

--- src/lib/brand.js
+++ src/lib/brand.js
@@ -11,7 +11,7 @@ export const BRAND = {
   location: 'Lagos, Nigeria',
   compliance: 'NDPA 2023 Compliant',
   assessmentUrl: 'https://assessment.valoriainstitute.com/',
-  logo: '/valoria-logo.svg',
+  logo: '/valoria-original.png',
 }
 
 export const COLORS = {
