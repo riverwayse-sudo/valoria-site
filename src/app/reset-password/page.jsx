bdeb061d63f20f80edad1d25dab54d35e87130fa
riverwayse-sudo <riverwayse@gmail.com>
2026-09-21 15:32:06 +0100

    brand: use exact original Valoria logo

--- src/app/reset-password/page.jsx
+++ src/app/reset-password/page.jsx
@@ -78,7 +78,7 @@ export default function ResetPasswordPage() {
         </div>
 
         <Link href="/" style={{ display: 'flex', justifyContent: 'center', marginBottom: '28px' }}>
-          <img src="/valoria-logo.svg" alt="Valoria Institute" style={{ height: '44px', width: 'auto' }} />
+          <img src="/valoria-original.png" alt="Valoria Institute" style={{ height: '44px', width: 'auto' }} />
         </Link>
 
         {done ? (
